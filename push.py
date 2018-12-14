eb9de6013577af5f30042b20940db917bae368bd
ZimingYuan <yzmyzm99@outlook.com>
2018-12-14 20:15:15 +0800

    Add title background texture.

--- push.py
+++ push.py
@@ -1,6 +1,6 @@
 import os
 
 os.system('git add .')
-s = input("Please input commit")
+s = input("Please input commit:\n")
 os.system('git commit -m \"' + s + '\"')
 os.system('git push origin master')
